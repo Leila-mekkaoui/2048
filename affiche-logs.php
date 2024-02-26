1b4b0bbf69e59cdbede654f8474fe603192f8497
Leila <82718511+Leila-mekkaoui@users.noreply.github.com>
2024-02-26 01:07:43 +0100

    logs
    fichier qui fait afficher // contient les logs

---
+++ affiche-logs.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>2048</title>
+    <meta http-equiv="refresh" content="5" />
+    <?php require_once 'fonctions-2048.php'; ?>
+</head>
+<body>
+	<?php
+		affiche_logs(5);
+	?>
+	
+	
+</body>
+</html>
